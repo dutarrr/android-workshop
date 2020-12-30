74ac5e8c42bb3a010c34790ad5544e70591e8566
dutarrr <duzguntutar@gmail.com>
2020-12-30 15:47:38 +0300

    listview onitemclick listener

--- app/src/main/java/com/dutar/myapplication/ListviewActivity.java
+++ app/src/main/java/com/dutar/myapplication/ListviewActivity.java
@@ -3,8 +3,11 @@ package com.dutar.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +31,15 @@ public class ListviewActivity extends AppCompatActivity {
 
         ArrayAdapter adapter = new ArrayAdapter(ListviewActivity.this,android.R.layout.simple_list_item_1,animals);
         listView.setAdapter(adapter);
+
+        //listview item click event
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast toast = Toast.makeText(getApplicationContext(),"Tıklanan item index: "+position,Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
 
     }
 }
