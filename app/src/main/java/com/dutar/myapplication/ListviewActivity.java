5378d988ce323a15301d8748eaf75596a5c6747d
dutarrr <duzguntutar@gmail.com>
2020-12-30 15:18:49 +0300

    listview added

--- app/src/main/java/com/dutar/myapplication/ListviewActivity.java
+++ app/src/main/java/com/dutar/myapplication/ListviewActivity.java
@@ -3,6 +3,11 @@ package com.dutar.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListviewActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class ListviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listview);
+
+        ListView listView = findViewById(R.id.listview);
+
+        List<String> animals = new ArrayList<>();
+        animals.add("Aslan");
+        animals.add("Fare");
+        animals.add("Ceylan");
+        animals.add("Yılan");
+        animals.add("Ayı");
+        animals.add("Kurt");
+
+        ArrayAdapter adapter = new ArrayAdapter(ListviewActivity.this,android.R.layout.simple_list_item_1,animals);
+        listView.setAdapter(adapter);
+
     }
 }
